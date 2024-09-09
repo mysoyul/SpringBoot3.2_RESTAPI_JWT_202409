946b1acd9c4b015f29479b4cfed89866d23bdb24
mysoyul <vega2k_@naver.com>
2024-09-09 15:41:43 +0900

    ErrorsSerializer add

--- src/main/java/com/boot3/myrestapi/lectures/LectureController.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -31,7 +31,7 @@ public class LectureController {
     public ResponseEntity<?> createLecture(@RequestBody @Valid LectureReqDto lectureReqDto,
                                            Errors errors) {
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(errors);
         }
         //Dto => Entity Convert
         Lecture lecture = modelMapper.map(lectureReqDto, Lecture.class);
