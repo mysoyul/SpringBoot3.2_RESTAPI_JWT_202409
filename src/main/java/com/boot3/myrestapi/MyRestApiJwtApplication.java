281f0b11fe6bf65c47f693d7775bd8d2da8f8a13
mysoyul <vega2k_@naver.com>
2024-09-09 10:50:00 +0900

    springboot restapi first

---
+++ src/main/java/com/boot3/myrestapi/MyRestApiJwtApplication.java
@@ -0,0 +1,13 @@
+package com.boot3.myrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestApiJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestApiJwtApplication.class, args);
+	}
+
+}
