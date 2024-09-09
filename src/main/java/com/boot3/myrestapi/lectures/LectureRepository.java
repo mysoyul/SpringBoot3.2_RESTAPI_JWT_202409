abf30879d652f7509bf1b22edbfb652ac42f141c
mysoyul <vega2k_@naver.com>
2024-09-09 13:30:54 +0900

    LectureRepository finder method add

--- src/main/java/com/boot3/myrestapi/lectures/LectureRepository.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureRepository.java
@@ -2,5 +2,11 @@ package com.boot3.myrestapi.lectures;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface LectureRepository extends JpaRepository<Lecture, Integer> {
+    List<Lecture> findByNameContaining(String name);
+    List<Lecture> findByBeginLectureDateTimeBetween (LocalDateTime time1,
+                                                     LocalDateTime tim2);
 }
