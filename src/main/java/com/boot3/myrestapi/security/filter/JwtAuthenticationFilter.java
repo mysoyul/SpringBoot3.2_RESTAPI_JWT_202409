d246c34de1a5b9270984a7c8ecc78b1dad1c46d7
mysoyul <vega2k_@naver.com>
2024-09-10 16:26:37 +0900

    SecurityConfig sessionmgt,addFilter

--- src/main/java/com/boot3/myrestapi/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/boot3/myrestapi/security/filter/JwtAuthenticationFilter.java
@@ -23,6 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private UserInfoUserDetailsService userDetailsService;
 
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
