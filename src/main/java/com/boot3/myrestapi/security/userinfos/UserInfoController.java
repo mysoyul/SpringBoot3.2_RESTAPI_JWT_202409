9c84107b1558ff84a5daf377c5e12b96b159f411
mysoyul <vega2k_@naver.com>
2024-09-10 15:50:03 +0900

    UserInfoController login add

--- src/main/java/com/boot3/myrestapi/security/userinfos/UserInfoController.java
+++ src/main/java/com/boot3/myrestapi/security/userinfos/UserInfoController.java
@@ -1,8 +1,14 @@
 package com.boot3.myrestapi.security.userinfos;
 
+import com.boot3.myrestapi.security.jwt.JwtService;
 import com.boot3.myrestapi.security.userinfos.domain.UserInfo;
 import com.boot3.myrestapi.security.userinfos.domain.UserInfoRepository;
+import com.boot3.myrestapi.security.userinfos.dto.AuthRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +19,11 @@ public class UserInfoController {
     private UserInfoRepository repository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -26,5 +37,18 @@ public class UserInfoController {
         return savedUserInfo.getName() + " user added!!";
     }
 
+    @PostMapping("/login")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getEmail(),
+                        authRequest.getPassword()
+                ));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getEmail());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+    }
 
 }
